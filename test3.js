2ba938d170593010ed934bf7fbde3c94983344fe
AplinXy9plin <evilberry@icloud.com>
2018-11-12 20:53:51 +0700

    Update test3.js

---
+++ test3.js
@@ -0,0 +1,17 @@
+var x = 1000, check = false;
+while(true){
+  x++
+  for (var i = 2; i < x; i++) {
+    // остаток от деления == 0
+    if(x%i == 0){
+      check = false
+      break
+    }else{
+      check = true
+    }
+  }
+  if(check == true){
+    console.log(x);
+    break
+  }
+}
